6926bf2e4d7f04f65bb6864f96998c01ef732d50
Arnav G <73305591+arnavg115@users.noreply.github.com>
2022-07-30 21:59:10 -0700

    HCO NN

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,44 @@ function slope(x: number) {
   return 0.5 * Math.cos(2 * Math.PI * x);
 }
 
+class HCONN {
+  collection = [
+    new HCO(150, 0),
+    new HCO(300, 0.25),
+    new HCO(450, 0.5),
+    new HCO(600, 0.75),
+  ];
+
+  step() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let x = 0; x < this.collection.length; x++) {
+      const y = this.collection[x];
+
+      y.step();
+    }
+  }
+}
+
 class HCO {
   p = 0;
   r = 0.5;
   omega = 0.1;
+  x = 150;
+
+  constructor(x: number, p: number) {
+    this.x = x;
+    this.p = p;
+    this.r = (p + 0.5) % 1;
+  }
+
   update() {
     this.p = (this.omega + this.p) % 1;
 
     this.r = (this.omega + this.r) % 1;
   }
-  animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  animate() {
     ctx.strokeStyle = "black";
     ctx.lineWidth = 1;
 
@@ -29,25 +55,25 @@ class HCO {
 
     ctx.fillStyle = `rgba(0,255,0,${p_1})`;
     ctx.beginPath();
-    ctx.ellipse(150, 100, 50, 50, 0, -Math.PI, Math.PI);
+    ctx.ellipse(this.x, 100, 50, 50, 0, -Math.PI, Math.PI);
     ctx.stroke();
     ctx.fill();
 
     // console.log(this.r);
     ctx.fillStyle = `rgba(0,255,0,${r_1})`;
     ctx.beginPath();
-    ctx.ellipse(150, 225, 50, 50, 0, -Math.PI, Math.PI);
+    ctx.ellipse(this.x, 225, 50, 50, 0, -Math.PI, Math.PI);
     ctx.stroke();
     ctx.fill();
 
     // Matrix transformation
-    ctx.translate(150, 300);
+    ctx.translate(this.x, 300);
     ctx.rotate(slope(this.p));
-    ctx.translate(-150, -300);
+    ctx.translate(-this.x, -300);
 
     // Rotated rectangle
     ctx.fillStyle = "red";
-    ctx.fillRect(145, 300, 10, 140);
+    ctx.fillRect(this.x - 5, 300, 10, 140);
 
     // ctx.translate(145, 100);
     // ctx.rotate(slope(this.p));
@@ -75,7 +101,7 @@ class HCO {
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 
-let h = new HCO();
+let h = new HCONN();
 function stp() {
   h.step();
   setTimeout(stp, 100);
